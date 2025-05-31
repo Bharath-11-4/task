82f908f4e9dc49ee7640559a5964382b3e1c53d2
Bharath-11-4 <9922005075@klu.ac.in>
2025-05-31 15:31:08 +0530

    intial commit

--- factorial.java
+++ factorial.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class factorial {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter the  factorial number ");
+        System.out.println("enter the number ");
         int a = sc.nextInt();
         int c=1;
         for (int i=1;i<=a;i++)
